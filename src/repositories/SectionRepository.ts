8facbba738dfb7462a1ae53dcff540417da3c48c
Jeremy Zongker <jeremy@trilitech.com>
2021-07-07 15:11:58 -0500

    Added notes and materials

--- src/repositories/SectionRepository.ts
+++ src/repositories/SectionRepository.ts
@@ -10,15 +10,15 @@ export class SectionRepository {
 
   public async create(section: Section) {
     section.id = UniqueIdHelper.shortId();
-    const sql = "INSERT INTO sections (id, churchId, lessonId, venueId, name, sort) VALUES (?, ?, ?, ?, ?, ?);";
-    const params = [section.id, section.churchId, section.lessonId, section.venueId, section.name, section.sort];
+    const sql = "INSERT INTO sections (id, churchId, lessonId, venueId, name, sort, materials) VALUES (?, ?, ?, ?, ?, ?, ?);";
+    const params = [section.id, section.churchId, section.lessonId, section.venueId, section.name, section.sort, section.materials];
     await DB.query(sql, params);
     return section;
   }
 
   public async update(section: Section) {
-    const sql = "UPDATE sections SET lessonId=?, venueId=?, name=?, sort=? WHERE id=? AND churchId=?";
-    const params = [section.lessonId, section.venueId, section.name, section.sort, section.id, section.churchId];
+    const sql = "UPDATE sections SET lessonId=?, venueId=?, name=?, sort=?, materials=? WHERE id=? AND churchId=?";
+    const params = [section.lessonId, section.venueId, section.name, section.sort, section.materials, section.id, section.churchId];
     await DB.query(sql, params);
     return section;
   }
