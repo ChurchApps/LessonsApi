452fca374607f64340f1b9ac061f3e7b7a95899e
Pranav Malvawala <pranavmalvawala@gmail.com>
2022-09-24 00:17:35 +0530

    fix

--- src/repositories/ExternalVideoRepository.ts
+++ src/repositories/ExternalVideoRepository.ts
@@ -44,7 +44,7 @@ export class ExternalVideoRepository {
   }
 
   public tempLoadNeedingUpdate(): Promise<ExternalVideo[]> {
-    return DB.query("select * from externalVideos limit 40 offset 160", []);
+    return DB.query("select * from externalVideos limit 40 offset 200", []);
   }
 
   public delete(churchId: string, id: string): Promise<ExternalVideo> {
