9aa0e98e468afe1443662c3189e050b7c5b67738
Jeremy Zongker <jeremy@zongker.net>
2026-06-10 01:01:45 -0500

    Updated helpers

--- src/helpers/Environment.ts
+++ src/helpers/Environment.ts
@@ -1,9 +1,4 @@
-import fs from "fs";
-import path from "path";
-import { fileURLToPath } from "url";
 import { AwsHelper, EnvironmentBase } from "@churchapps/apihelper";
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export class Environment extends EnvironmentBase {
   static transcodePipeline: string;
@@ -16,23 +11,11 @@ export class Environment extends EnvironmentBase {
   static mauticPassword: string;
 
   static async init(environment: string) {
-    let file = "dev.json";
-    if (environment === "demo") file = "demo.json";
-    if (environment === "staging") file = "staging.json";
-    if (environment === "prod") file = "prod.json";
-    if (environment === "selfhost") file = "selfhost.json";
-
-    const relativePath = "../../config/" + file;
-    const physicalPath = path.resolve(__dirname, relativePath);
-
-    const json = fs.readFileSync(physicalPath, "utf8");
-    const data = JSON.parse(json);
+    const data = await this.initBase(environment, { appName: "lessonsApi", fileMap: { selfhost: "selfhost.json" } });
 
     if (environment === "selfhost") {
-      data.contentRoot = process.env.CONTENT_ROOT || data.contentRoot;
+      this.contentRoot = process.env.CONTENT_ROOT || this.contentRoot;
     }
-
-    await this.populateBase(data, "lessonsApi", environment);
 
     this.transcodePipeline = data.transcodePipeline;
     this.transcodePreset = data.transcodePreset;
